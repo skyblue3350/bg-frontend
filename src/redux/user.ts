f42adb46b4d1aeb538d4890060585d81963f540c
sky <skyblue3350@gmail.com>
2019-03-18 05:12:04 +0900

    redux thunk用に変更

--- src/redux/user.ts
+++ src/redux/user.ts
@@ -1,46 +1,98 @@
-export const LOGIN = "LOGIN"
+export const LOGIN_TRIES = "LOGIN_TRIES"
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const LOGIN_FAIL = "LOGIN_FAIL"
 export const LOGOUT = "LOGOUT"
 
 export interface UserState {
     uid: string
+    pass: string
     jwt: string
+    isFetch: boolean
+    message: string
 }
 
-interface LoginAction {
-    type: typeof LOGIN
+interface LoginAction_TRIES {
+    type: typeof LOGIN_TRIES
+}
+
+interface LoginAction_SUCCESS {
+    type: typeof LOGIN_SUCCESS
     payload: UserState
 }
+
+interface LoginAction_FAIL {
+    type: typeof LOGIN_FAIL
+    message: string
+}
+
 interface LogoutAction {
     type: typeof LOGOUT
 }
-type UserAction = LoginAction | LogoutAction
+type UserAction = LoginAction_TRIES | LoginAction_SUCCESS | LoginAction_FAIL | LogoutAction
 
 const initialState: UserState = {
     uid: null,
+    pass: null,
     jwt: null,
+    isFetch: false,
+    message: null
 }
 
 // Reducer
 export default function UserReducer(state=initialState, action: UserAction): UserState {
+    console.log(action.type);
     switch (action.type) {
-        case LOGIN:
+        case LOGIN_TRIES:
             return {
+                ...state,
+                isFetch: true,
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
                 uid: action.payload.uid,
-                jwt: action.payload.jwt
+                jwt: action.payload.jwt,
+                isFetch: false,
+            }
+        case LOGIN_FAIL:
+            return {
+                ...state,
+                message: action.message,
+                isFetch: false,
+
             }
         case LOGOUT:
             return initialState
-        default: {
+        default:
             return state
-        }
     }
 }
 
 // Action
-export function login(payload: UserState){
+export interface UserActions {
+    login: (uid: string, pass: string) => void
+}
+
+export function fetch() {
     return {
-        type: LOGIN,
-        payload
+        type: LOGIN_TRIES,
+    }
+}
+
+export function fail(message: string) {
+    return {
+        type: LOGIN_FAIL,
+        message: message,
+    }
+}
+
+export function login(uid: string, jwt: string){
+    return {
+        type: LOGIN_SUCCESS,
+        payload: {
+            uid,
+            jwt,
+        }
     }
 }
 
